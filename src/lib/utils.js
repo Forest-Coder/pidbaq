1b69b66e15c2d6248dcf4a7e2f05e1666d17391d
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-13 16:38:02 +0900

    replaced supabase

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,21 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const addFeedbackURL = import.meta.env.VITE_ADD_FEEDBACK_URL;
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
+}
+
+export async function addFeedback(data){
+  console.log(addFeedbackURL)
+  return await fetch(addFeedbackURL, {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).catch(err => {
+    console.error(err)
+  })
 }
